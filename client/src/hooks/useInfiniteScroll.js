c63dc1525eaa504e2843635e16a4bfa321d7b28a
Mike Barkmin <mike.barkmin@uni-due.de>
2019-09-08 11:48:47 +0200

    Add InfiniteScrool hook

---
+++ client/src/hooks/useInfiniteScroll.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+
+const useInfiniteScroll = callback => {
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  useEffect(() => {
+    if (!isFetching) return;
+    callback(() => {
+      console.log('called back');
+    });
+  }, [isFetching, callback]);
+
+  function handleScroll() {
+    if (
+      window.innerHeight + document.documentElement.scrollTop !==
+        document.documentElement.offsetHeight ||
+      isFetching
+    )
+      return;
+    setIsFetching(true);
+  }
+
+  return [isFetching, setIsFetching];
+};
+
+export default useInfiniteScroll;
