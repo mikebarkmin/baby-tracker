c1ac10b71e9d8a5411783ba7ff4eccc30021b977
Mike Barkmin <mike.barkmin@uni-due.de>
2019-09-24 20:55:51 +0200

    Add sentry

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+Sentry.init({
+  dsn: 'https://a03f711acaf54badbfa281a9b78b42e0@sentry.io/1760312'
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
