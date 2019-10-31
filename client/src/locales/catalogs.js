942a41bf21b59f150c74fe5485424be6ab162dd8
PTST <17211264+PTST@users.noreply.github.com>
2019-10-31 08:47:01 +0100

    Update catalogs.js

--- client/src/locales/catalogs.js
+++ client/src/locales/catalogs.js
@@ -1,9 +1,11 @@
 import deDE from './de-DE/messages';
+import daDK from './da-DK/messages';
 import enUS from './en-US/messages';
 import fr from './fr/messages';
 
 export default {
   deDE,
+  daDK,
   enUS,
   fr
 };
