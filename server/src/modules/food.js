ab2e56b0c97f3dd287d0c6051f0359121b34ac9e
Mike Barkmin <mike.barkmin@uni-due.de>
2020-01-09 11:12:45 +0100

    Fix getTypes not respecting privacy

--- server/src/modules/food.js
+++ server/src/modules/food.js
@@ -18,7 +18,12 @@ export const Model = model('Food', schema);
 export function handler(socket) {
   eventHandler(socket, 'food', Model);
   socket.on('food/getTypes', function(callback) {
-    Model.find()
+    if (!socket.baby) {
+      callback({ msg: 'no baby' });
+      return;
+    }
+    const babyId = socket.baby._id;
+    Model.find({ babyId })
       .distinct('type')
       .then(docs => {
         callback({ msg: 'success', types: docs });
