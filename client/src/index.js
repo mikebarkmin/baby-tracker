7e9edfa77836866f0db626c2fba8961a7728717f
Mike Barkmin <mike@barkmin.eu>
2020-12-29 20:49:05 +0100

    configure sentry with env

--- client/src/index.js
+++ client/src/index.js
@@ -1,29 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as Sentry from '@sentry/browser';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/browser";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-Sentry.init({
-  dsn: 'https://a03f711acaf54badbfa281a9b78b42e0@sentry.io/1760312'
-});
+if (process.env.REACT_APP_SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+  });
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register({
-  onUpdate: registration => {
-    const event = new CustomEvent('sw-updated');
+  onUpdate: (registration) => {
+    const event = new CustomEvent("sw-updated");
     window.dispatchEvent(event, {
-      registration
+      registration,
     });
   },
-  onSuccess: registration => {
-    const event = new CustomEvent('sw-installed');
+  onSuccess: (registration) => {
+    const event = new CustomEvent("sw-installed");
     window.dispatchEvent(event, {
-      registration
+      registration,
     });
-  }
+  },
 });
