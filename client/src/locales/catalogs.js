254c55b4cb1cec6e420f10650eeb4576e1b3f0b7
Andy Tang <winterwind2022@gmail.com>
2021-01-26 12:34:39 -0600

    add mandarin chinese (simplified) locale

--- client/src/locales/catalogs.js
+++ client/src/locales/catalogs.js
@@ -2,10 +2,12 @@ import deDE from './de-DE/messages';
 import daDK from './da-DK/messages';
 import enUS from './en-US/messages';
 import fr from './fr/messages';
+import zhCMN from './zh-CMN/messages';
 
 export default {
   deDE,
   daDK,
   enUS,
-  fr
+  fr,
+  zhCMN
 };
